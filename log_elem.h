c5690beed4023e51ecb4c94e8c3cb5cd59f4a028
glucogen19 <98939682+glucogen19@users.noreply.github.com>
2023-07-28 21:55:59 +0400

    Переопределение виртуальной функции Calculate

--- log_elem.h
+++ log_elem.h
@@ -9,15 +9,32 @@ public:
 };
 
 class AND : public LogicalElement {
+public:
+    AND(bool input1, bool input2);
+    bool Calculate() const override;
 
+private:
+    bool input1_;
+    bool input2_;
 };
 
 class OR : public LogicalElement {
+public:
+    OR(bool input1, bool input2);
+    bool Calculate() const override;
 
+private:
+    bool input1_;
+    bool input2_;
 };
 
 class NOT : public LogicalElement {
+public:
+    NOT(bool input);
+    bool Calculate() const override;
 
+private:
+    bool input_;
 };
 
 #endif 
