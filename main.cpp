20c4e5ef5099c7c0074179fe0db6587c2fb2d2fa
glucogen19 <98939682+glucogen19@users.noreply.github.com>
2023-07-28 22:01:29 +0400

    Вызов для каждого объекта метода Calculate()

--- main.cpp
+++ main.cpp
@@ -8,4 +8,10 @@ int main() {
 	AND andGate(input1, input2);
     OR orGate(input1, input2);
     NOT notGate(input1);
+	
+	std::cout << "AND gate result: " << andGate.Calculate() << std::endl;
+    std::cout << "OR gate result: " << orGate.Calculate() << std::endl;
+    std::cout << "NOT gate result (input 1): " << notGate.Calculate() << std::endl;
+
+    return 0;
 }
