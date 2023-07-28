343e096e1443677d54515ff93b15309204e10d43
glucogen19 <98939682+glucogen19@users.noreply.github.com>
2023-07-28 21:57:38 +0400

    Определение методов для классов AND, OR, NOT

--- log_elem.cpp
+++ log_elem.cpp
@@ -2,3 +2,18 @@
 
 LogicalElement::LogicalElement() {}
 LogicalElement::~LogicalElement() {}
+
+AND::AND(bool input1, bool input2) : input1_(input1), input2_(input2) {}
+bool AND::Calculate() const {
+    return input1_ && input2_;
+}
+
+OR::OR(bool input1, bool input2) : input1_(input1), input2_(input2) {}
+bool OR::Calculate() const {
+    return input1_ || input2_;
+}
+
+NOT::NOT(bool input) : input_(input) {}
+bool NOT::Calculate() const {
+    return !input_;
+}
